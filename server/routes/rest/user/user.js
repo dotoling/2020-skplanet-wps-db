711e64bea29be4f17d9612893dd5651138fd611c
Dowon Kwak <dowonkwak@Dowonui-MacBookPro.local>
2020-11-17 18:08:54 +0900

    [Edit] Append Output

--- server/routes/rest/user/user.js
+++ server/routes/rest/user/user.js
@@ -3,9 +3,11 @@ const models = require('../../../models');
 const config = require('../../../config/config.json')[process.env.NODE_ENV || 'development'];
 
 const curPos = async (req, res) => {
+    const outputCurPos = "result Position";
     console.log(req.body)
     console.log("========================");
     res.send({
+        curPosition : outputCurPos,
         result : true,
     })
 };
