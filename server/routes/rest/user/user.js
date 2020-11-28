ef517a197367b83d4c391f7d076bb48764d0bbb9
kangrnin <kangrnin@gmail.com>
2020-11-28 15:51:10 +0900

    move python spawn code to REST

--- server/routes/rest/user/user.js
+++ server/routes/rest/user/user.js
@@ -3,15 +3,32 @@ const models = require('../../../models');
 const config = require('../../../config/config.json')[process.env.NODE_ENV || 'development'];
 
 const curPos = async (req, res) => {
-    const outputCurPos = "result Position";
-    console.log(req.body)
-    console.log("========================");
     res.send({
-        curPosition : outputCurPos,
+        curPos : "롯데백화점",
         result : true,
     })
 };
-  
-  module.exports= {
-      curPos,
-  }
+
+const curRp = async (req, res) => {
+    const wifi_data = [];
+    const {spawn} = require('child_process');
+    const python = spawn('python', ['../ml/detect.py', wifi_data]);
+
+    /* argv로 wifi_data가 커서 안넘어가면 이걸로 대체
+    const wifi_data = JSON.stringify([1,2,3,4,5]);
+    python.stdin.write(data);
+    python.stdin.end();
+    */
+
+    python.stdout.on('data', (data) => {
+        res.send({
+            curRp : data.toString(),
+            result : true,
+        }
+    );
+    python.on('close', (code) => {});
+};
+
+module.exports= {
+    curPos, curRp
+};
