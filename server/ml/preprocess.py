5937187c3fd7b633e1a7ee74ab1f1644b7766a5a
kangrnin <kangrnin@gmail.com>
2020-11-29 17:04:11 +0900

    Python : load data and arguments, input/output encoding

--- server/ml/preprocess.py
+++ server/ml/preprocess.py
@@ -1,2 +1,18 @@
+import sys, json
+
+import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from sklearn.preprocessing import LabelEncoder
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 lines = sys.stdin.readlines()
-user_wifi = json.loads(lines[0])[0]
+
+user_wifi = json.loads(lines[0])
+pos_name, lat, lon = sys.argv[1:4]
+
+df = pd.DataFrame.from_records(user_wifi)
+print(df)
--- server/scripts/addPos_test.js
+++ server/scripts/addPos_test.js
@@ -1,12 +1,15 @@
 const { wifi_data, pos_name, lat, lon } = {
   wifi_data: [
-      {bssid: "12:34", rssi: 80},
-      {bssid: "56:78", rssi: 56},
-      {bssid: "54:53", rssi: 56}
+      {bssid: '12:34', rssi: 80, timestamp: '2020-11-26 21:30:22.159452', position: '롯데백화점', rp: '2층 입구'},
+      {bssid: '56:78', rssi: 56, timestamp: '2020-11-26 21:30:22.159452', position: '롯데백화점', rp: '2층 입구'},
+      {bssid: '54:53', rssi: 57, timestamp: '2020-11-26 21:30:22.159452', position: '롯데백화점', rp: '2층 입구'},
+      {bssid: '54:a2', rssi: 54, timestamp: '2020-11-26 22:33:22.159452', position: '롯데백화점', rp: 'ZARA'},
+      {bssid: '54:dc', rssi: 43, timestamp: '2020-11-26 22:33:22.159452', position: '롯데백화점', rp: 'ZARA'},
+      {bssid: '54:ad', rssi: 97, timestamp: '2020-11-26 22:33:22.159452', position: '롯데백화점', rp: 'ZARA'},
   ],
-  pos_name: "test_pos",
-  lat: "37.296429",
-  lon: "126.971933"
+  pos_name: 'test_pos',
+  lat: '37.296429',
+  lon: '126.971933'
 };
 
 var path = require('path');
@@ -15,21 +18,19 @@ const {spawn} = require('child_process');
 const py_path = path.join(__dirname, '../ml/preprocess.py');
 const py = spawn('python', [py_path, pos_name, lat, lon]);
 
-py.stdout.on('data', (data) => {
-    console.log(String.fromCharCode.apply(null, data));
-});
-py.stderr.on('data', (data) => {
-    console.log(String.fromCharCode.apply(null, data));
-});
+py.stdout.setEncoding('utf-8');
+py.stdout.on('data', data=>console.log(data));
+py.stderr.on('data', data=>console.log(data));
+py.stdin.setDefaultEncoding('utf-8');
+py.stdin.write(JSON.stringify(wifi_data));
+py.stdin.end();
 
-py.on('close', (code) => {
+py.on('end', (code) => {
   const py2_path = path.join(__dirname, '../ml/preprocess.py');
   const py2 = spawn('python', [py2_path, pos_name, lat, lon]);
 
-  py2.on('close', (code) => {
+  py2.on('end', (code) => {
     //res.send({ result: true });
-    console.log("SUCCESS");
+    console.log('SUCCESS');
   })
 });
-py.stdin.write(JSON.stringify(wifi_data));
-py.stdin.end();
