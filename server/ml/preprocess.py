3fcc57003c4bfe5d3ef4159e88d83479fcc027f5
kangrnin <kangrnin@gmail.com>
2020-11-29 21:31:41 +0900

    Added logging, removed pos_name in preprocess.py

--- server/ml/preprocess.py
+++ server/ml/preprocess.py
@@ -1,12 +1,13 @@
 import sys, json
-
 import os
+import logging as log
 from pathlib import Path
+from datetime import datetime
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
-from collections import defaultdict
 from sklearn.preprocessing import LabelEncoder
-from datetime import datetime
 
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
@@ -14,20 +15,25 @@ sys.stderr.reconfigure(encoding='utf-8')
 lines = sys.stdin.readlines()
 
 user_wifi = json.loads(lines[0])
-pos_name, lat, lon = sys.argv[1:4]
+lat, lon = sys.argv[1:3]
 
 rssi_threshold = 40
 
-df_wifi = pd.DataFrame.from_records(user_wifi)
-df_wifi = df_wifi[df_wifi['rssi'] > rssi_threshold]
+df_pos = pd.DataFrame.from_records(user_wifi)
+df_pos = df_pos[df_pos['rssi'] > rssi_threshold]
+pos_name = df_pos['position'][0]
+
+log_path = Path(__file__).parent / 'log'
+log_path.mkdir(parents=True, exist_ok=True)
+
+handler = log.FileHandler(log_path / 'preprocess.log', 'a+', 'utf-8')
+log.basicConfig(handlers=[handler], level=log.INFO)
 
 cur_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-
 # 전체 Wi-Fi 데이터 목록에서 rp 별로 분리
-for rp_name, df_rp in df_wifi.groupby('rp'):
-
-    data_path = Path(__file__).parent / 'data' / pos_name / rp_name
-    data_path.mkdir(parents=True, exist_ok=True)
+for rp_name, df_rp in df_pos.groupby('rp'):
+    rp_path = Path(__file__).parent / 'data' / pos_name / rp_name
+    rp_path.mkdir(parents=True, exist_ok=True)
 
     # defaultdict.fromkeys()에서 알아서 중복 제거해줌
     # key = timestamp, value = defaultdict of bssid in rp : rssi, rp : no. of rp
@@ -39,5 +45,8 @@ for rp_name, df_rp in df_wifi.groupby('rp'):
     train_data = pd.DataFrame.from_dict(dict_rp).transpose()
 
     time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    train_data.to_csv(data_path / f'{cur_time}.csv', index=False)
+    train_data.to_csv(rp_path / f'{cur_time}.csv', index=False)
+    log.info(f'{datetime.now()} train data generated for {pos_name} | {rp_name} | {cur_time}')
+
+
 
