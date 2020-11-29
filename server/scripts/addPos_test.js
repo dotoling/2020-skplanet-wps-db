b9cd35749c2f9684ae70456be413cd6ad1b0483d
kangrnin <kangrnin@gmail.com>
2020-11-29 21:10:38 +0900

    Remove pos_name in addPos request body

--- server/scripts/addPos_test.js
+++ server/scripts/addPos_test.js
@@ -1,4 +1,4 @@
-const { wifi_data, pos_name, lat, lon } = {
+const { wifi_data, lat, lon } = {
   wifi_data: [
       {bssid: '12:34', rssi: 80, timestamp: '2020-11-26 21:30:22.159452', position: '롯데백화점', rp: '2층 입구'},
       {bssid: '56:78', rssi: 56, timestamp: '2020-11-26 21:30:22.159452', position: '롯데백화점', rp: '2층 입구'},
@@ -7,7 +7,6 @@ const { wifi_data, pos_name, lat, lon } = {
       {bssid: '54:dc', rssi: 43, timestamp: '2020-11-26 22:33:22.159452', position: '롯데백화점', rp: 'ZARA'},
       {bssid: '54:ad', rssi: 97, timestamp: '2020-11-26 22:33:22.159452', position: '롯데백화점', rp: 'ZARA'},
   ],
-  pos_name: 'test_pos',
   lat: '37.296429',
   lon: '126.971933'
 };
@@ -16,18 +15,19 @@ var path = require('path');
 
 const {spawn} = require('child_process');
 const py_path = path.join(__dirname, '../ml/preprocess.py');
-const py = spawn('python', [py_path, pos_name, lat, lon]);
+const py = spawn('python', [py_path, lat, lon]);
 
+py.stdin.setDefaultEncoding('utf-8');
 py.stdout.setEncoding('utf-8');
+py.stderr.setEncoding('utf-8');
 py.stdout.on('data', data=>console.log(data));
 py.stderr.on('data', data=>console.log(data));
-py.stdin.setDefaultEncoding('utf-8');
 py.stdin.write(JSON.stringify(wifi_data));
 py.stdin.end();
 
 py.on('end', (code) => {
   const py2_path = path.join(__dirname, '../ml/preprocess.py');
-  const py2 = spawn('python', [py2_path, pos_name, lat, lon]);
+  const py2 = spawn('python', [py2_path, lat, lon]);
 
   py2.on('end', (code) => {
     //res.send({ result: true });
