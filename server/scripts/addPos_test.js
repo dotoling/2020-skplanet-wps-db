dfddf6b9750f2e79be4e328fcd7cdd29a87afe75
kangrnin <kangrnin@gmail.com>
2020-11-29 15:35:04 +0900

    python spawn test input/output

--- server/scripts/addPos_test.js
+++ server/scripts/addPos_test.js
@@ -1,13 +1,30 @@
-const { wifi_data, pos_name, lat, lon } = {};
-if(!wifi_data || !pos_name || !lat || !lon) {
-    //res.send({ result : false });
-}
+const { wifi_data, pos_name, lat, lon } = {
+  wifi_data: [
+      {bssid: "12:34", rssi: 80},
+      {bssid: "56:78", rssi: 56},
+      {bssid: "54:53", rssi: 56}
+  ],
+  pos_name: "test_pos",
+  lat: "37.296429",
+  lon: "126.971933"
+};
+
+var path = require('path');
 
 const {spawn} = require('child_process');
-const py = spawn('python', ['../../../ml/preprocess.py', pos_name, lat, lon]);
+const py_path = path.join(__dirname, '../ml/preprocess.py');
+const py = spawn('python', [py_path, pos_name, lat, lon]);
+
+py.stdout.on('data', (data) => {
+    console.log(String.fromCharCode.apply(null, data));
+});
+py.stderr.on('data', (data) => {
+    console.log(String.fromCharCode.apply(null, data));
+});
 
 py.on('close', (code) => {
-  const py2 = spawn('python', ['../../../ml/train.py', pos_name, lat, lon]);
+  const py2_path = path.join(__dirname, '../ml/preprocess.py');
+  const py2 = spawn('python', [py2_path, pos_name, lat, lon]);
 
   py2.on('close', (code) => {
     //res.send({ result: true });
