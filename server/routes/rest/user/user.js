151ef263f3ab729fab69699f4785803693d2f26c
kangrnin <kangrnin@gmail.com>
2020-12-06 13:31:03 +0900

    curPos send result when detection ended

--- server/routes/rest/user/user.js
+++ server/routes/rest/user/user.js
@@ -15,31 +15,36 @@ const curPos = async (req, res) => {
     // });
 
     // add check-in data in db_checkIn table
-    models.db_checkIn.create({
-        checkInPos : curPosName,
-    })
+    // models.db_checkIn.create({
+    //     checkInPos : curPosName,
+    // })
     if(!wifi_data || !lat || !lon)
         res.send({ result : false });
 
     const {spawn} = require('child_process');
     const py_path = path.join(__dirname, '../../../ml/detect.py');
     const py = spawn('python', [py_path, "롯데백화점"]);
+    let result = '';
     
     py.stdin.setDefaultEncoding('utf-8');
     py.stdout.setEncoding('utf-8');
     py.stderr.setEncoding('utf-8');
     py.stdout.on('data', (data) => {
-        res.send({
-            // "롯데백화점 ZARA" 형식으로 출력됨
-            curRp : data.toString(),
-            result : true,
+        result += data.toString();
     });
     py.stdin.write(JSON.stringify(wifi_data));
     py.stdin.end();
-
-	models.db_checkIn.create({
-       		checkInPos : data.toString(),
-    	});
+    
+    py.on('exit', (code) => {
+        console.log(result);
+        res.send({
+            // "롯데백화점 ZARA" 형식으로 출력됨
+            curRp : result,
+            result : true,
+        });
+        models.db_checkIn.create({
+            checkInPos : result,
+        });
     });
 };
 
